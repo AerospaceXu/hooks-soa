25cfcfd07f1acf3a64e61a1a11c8f720174d2b45
AerospaceXu <aerospacexu@gmail.com>
2021-01-14 21:37:09 +0800

    feat: Add useAnime hook.

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,34 @@ import styled from 'styled-components';
 
 import {
   GlobalService,
-  useRootGlobalService
+  useRootGlobalService,
 } from './app/services/global.service';
 
 import Button from './app/components/Button';
 import Panel from './app/components/Panel';
+import Animate from './app/components/Animate/Animate';
 
 const Wrapper = styled.div`
   width: 200px;
   margin: 88px auto;
+
+  > .counter {
+    width: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `;
 
 const App: React.FC = () => {
   return (
     <GlobalService.Provider value={useRootGlobalService()}>
       <Wrapper>
-        <Button />
-        <Panel />
+        <div className="counter">
+          <Button />
+          <Panel />
+        </div>
+        <Animate />
       </Wrapper>
     </GlobalService.Provider>
   );
